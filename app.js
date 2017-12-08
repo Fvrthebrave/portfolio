e88fcd28471eaa8193e8c74cebbf82c28f88add4
fvrthebrave <collin.desoto@gmail.com>
2017-12-08 19:41:46 +0000

    change styling and add pages

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.render('index'); 
+});
+
+app.get('/portfolio', (req, res) => {
+   res.render('portfolio'); 
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact-form'); 
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server is listening...');
 });
